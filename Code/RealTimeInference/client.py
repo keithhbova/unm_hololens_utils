d52c7bcc6e2eae54385303a382fa5bc3f162871b
keithhbova <keithhbova@gmail.com>
2023-03-05 11:27:03 -0700

    rewrote using classes

--- Code/RealTimeInference/client.py
+++ Code/RealTimeInference/client.py
@@ -2,49 +2,55 @@ import cv2
 import socket
 import numpy as np
 
-# Define the IP address and port of the UDP sender
-UDP_IP:str = ''
-UDP_PORT:int = 6969
+class UdpVideoCapture:
+    def __init__(self, udp_ip, udp_port):
+        self.udp_ip = udp_ip
+        self.udp_port = udp_port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((udp_ip, udp_port))
+        self.max_packet_size = 2048
+        self.frame_chunks = []
 
-# Create a UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    def read(self):
+        while True:
+            # Receive a chunk of data
+            data, addr = self.sock.recvfrom(self.max_packet_size)
 
-# Bind the socket to the IP address and port
-sock.bind((UDP_IP, UDP_PORT))
+            # Decode the data into a numpy array
+            chunk = np.frombuffer(data, dtype=np.uint8)
 
-# Define the maximum packet size
-MAX_PACKET_SIZE = 1024
+            # Append the chunk to the list of frame chunks
+            self.frame_chunks.append(chunk)
 
-# Initialize the frame variable
-frame_chunks = []
+            # If we have received a full frame, decode it and return it
+            if len(chunk) < self.max_packet_size:
+                # Concatenate the frame chunks into a single numpy array
+                frame_data = np.concatenate(self.frame_chunks, axis=None)
+
+                # Decode the frame data into a numpy array
+                frame = cv2.imdecode(frame_data, cv2.IMREAD_COLOR)
+
+                # Reset the frame chunks list
+                self.frame_chunks = []
+
+                # Return the frame
+                return True, frame
+
+    def release(self):
+        self.sock.close()
+
+
+udp_capture = UdpVideoCapture('192.168.42.15', 6969)
 
 while True:
-    # Receive a chunk of data
-    data, addr = sock.recvfrom(MAX_PACKET_SIZE)
+    ret, frame = udp_capture.read()
 
-    # Decode the data into a numpy array
-    chunk = np.frombuffer(data, dtype=np.uint8)
-
-    # Append the chunk to the list of frame chunks
-    frame_chunks.append(chunk)
-
-    # If we have received a full frame, decode it and display it
-    if len(chunk) < MAX_PACKET_SIZE:
-        # Concatenate the frame chunks into a single numpy array
-        frame_data = np.concatenate(frame_chunks, axis=None)
-
-        # Decode the frame data into a numpy array
-        frame = cv2.imdecode(frame_data, cv2.IMREAD_COLOR)
-
-        # Display the frame
+    if ret:
         cv2.imshow('frame', frame)
 
-        # Press 'q' to quit
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-        # Reset the frame chunks list
-        frame_chunks = []
-
-# Release everything
+udp_capture.release()
 cv2.destroyAllWindows()
+
